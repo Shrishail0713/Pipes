a230d8f40e8f3f92fcf713fa52f6bc3d045055e0
Shrishail0713 <79127553+Shrishail0713@users.noreply.github.com>
2021-03-02 12:41:53 +0530

    Pipes

--- testwaitpid.c
+++ testwaitpid.c
@@ -4,8 +4,10 @@
 #include<sys/wait.h>
 
 int main() {
-   int status=0;
-   waitpid(,&status,0);
-   printf("exit status is %d",status);
+
+     int status=0;	 
+     waitpid(, &status, 0);	 
+	 printf("Exit Status is  %d",status);    
+  
    return 0;
 }
